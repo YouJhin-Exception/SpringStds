5cfea06497a3a041eddc259dcf7331e60d5392cb
«YouJhin» <palevich86@mail.ru>
2023-05-30 16:31:22 +0300

    Initial commit

---
+++ GhaDD/src/main/java/com/youjhin/stds1/Stds1Application.java
@@ -0,0 +1,13 @@
+package com.youjhin.stds1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stds1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stds1Application.class, args);
+	}
+
+}
