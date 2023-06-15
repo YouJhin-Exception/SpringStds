2f7543c181a3384410db8066e647d1eacea9d5b6
«YouJhin» <palevich86@mail.ru>
2023-06-15 14:08:49 +0300

    ex3 BuySell add SQL :mag_right:

--- yakunBuySell/src/main/java/com/example/yakunbuysell/services/ProductService.java
+++ yakunBuySell/src/main/java/com/example/yakunbuysell/services/ProductService.java
@@ -1,37 +1,34 @@
 package com.example.yakunbuysell.services;
 
 import com.example.yakunbuysell.models.Product;
+import com.example.yakunbuysell.repositories.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class ProductService {
-    private List<Product> allProducts = new ArrayList<>();
-    private long productId = 0;
+    private final ProductRepository productRepository;
 
-    {
-        allProducts.add(new Product(++productId,"PlayStation", "some Desc to PS",60000, "Stavropol","Jhin"));
-        allProducts.add(new Product(++productId,"Iphone12", "some Desc to iphone",80000, "Stavropol","Jhin"));
-    }
-    public List<Product> allProduct(){
-        return allProducts;
+    public List<Product> allProduct(String title){
+        if (title != null) return productRepository.findByTitle(title);
+        return productRepository.findAll();
     }
     public void saveProduct(Product product){
-        product.setId(++productId);
-        allProducts.add(product);
+        log.info("Saving new {}", product);
+        productRepository.save(product);
     }
 
     public void deleteProduct(Long id){
-        allProducts.removeIf(product -> product.getId().equals(id));
+        productRepository.deleteById(id);
     }
 
     public Product getProductById(Long id) {
-        for (Product product : allProducts) {
-            if (product.getId().equals(id))
-                return product;
-        }
-        return null;
+        return productRepository.findById(id).orElse(null);
     }
 }
